b45a2df81d006d95ced4be3bed3c8ac0d22c4485
E-K8 <ekatplat@gmail.com>
2022-02-10 15:49:45 +0000

    attempt to add component Toggle

--- vue-hello-world/src/components/HelloWorld.vue
+++ vue-hello-world/src/components/HelloWorld.vue
@@ -1,20 +1,19 @@
 <template>
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <p>Welcome to Vue</p>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
   data() {
     return {
-      count: 0
-    }
-  }
-}
+      count: 0,
+    };
+  },
+};
 </script>
-
